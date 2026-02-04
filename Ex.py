d39299c9a6c9d101eadbd12fd206738f539e274f
Lucas Estoco <lucasestoco22@gmail.com>
2026-02-04 17:36:01 -0300

    Add files via upload

---
+++ Ex.py
@@ -0,0 +1,14 @@
+def somar(a, b):
+    resultado = a + b
+    return resultado
+
+soma = somar(5, 3)
+print(soma)
+
+def contar_letras():
+    palavra = input("Digite uma palavra: ")
+    print(f"A quantidade de letras da palavra {palavra} é {len(palavra)}")
+    return palavra
+contar_letra
+    #asdasdasdasd
+    
